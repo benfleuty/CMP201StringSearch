52904033683094c6d8b6f37597733ffe59b8f1d5
Ben Fleuty <bfleuty@outlook.com>
2020-12-23 12:02:21 +0000

    multiple changes
    ~ Alter clear function to print current position's title
    + function to get text
    + function to get pattern
    ~ Alter RK to use above functions

--- CMP201StringSearch/CMP201StringSearch.cpp
+++ CMP201StringSearch/CMP201StringSearch.cpp
@@ -13,6 +13,7 @@
 /// From https://stackoverflow.com/questions/6486289/how-can-i-clear-console
 /// Answer by: Joma
 /// </summary>
+/*
 void Clear()
 {
 #if defined _WIN32
@@ -26,7 +27,41 @@ void Clear()
 #endif
     std::cout << "Ben Fleuty | CMP 201 Assessment | String search\n";
 }
+*/
 
+enum class MessageType {
+    Menu,
+    RabinKarp
+};
+
+void Clear(MessageType message)
+{
+#if defined _WIN32
+    system("cls");
+    //clrscr(); // including header file : conio.h
+#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+    //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
+#elif defined (__APPLE__)
+    system("clear");
+#endif
+
+    switch (message) {
+    case MessageType::Menu:
+        std::cout << "Ben Fleuty | CMP 201 Assessment | String search\n";
+        std::cout << "Main menu\n\n";
+        break;
+
+    case MessageType::RabinKarp:
+        std::cout << "-------------------------------\n";
+        std::cout << "|String search with Rabin Karp|\n";
+        std::cout << "-------------------------------\n";
+        break;
+
+    default:
+        std::cout << "Error in clear -> title printing";
+    }
+}
 
 std::string GetFileText(std::string fileToRead) {
     // read text file
@@ -42,42 +77,50 @@ std::string GetFileText(std::string fileToRead) {
     return output;
 }
 
-void RabinKarp() {
-    Clear();
-    std::cout << "-------------------------------\n";
-    std::cout << "|String search with Rabin Karp|\n";
-    std::cout << "-------------------------------\n";
-    // get text file
+std::string GetText() {
+    //// get text file
     std::string input;
     do {
-        std::cout << "Type y when the text you want to search is in a text file named \"search.txt\" in the folder that this .exe is in";
+        std::cout << "Enter y when the text you want to search is in a text file named \"search.txt\" in the same folder as this .exe\n";
         std::getline(std::cin, input);
     } while (input[0] != 'y');
-
-
-    // text to search
-    std::string text;
-
-    // temp file to store each line
-    std::string line;
-
-    std::ifstream fileToRead("search.txt");
-
-    // iterate each line in the file and add the line to the text to search
-    while (std::getline(fileToRead, line)) text.append(line);
-
-
-    
-
+    return GetFileText("search.txt");
 }
 
+std::string GetPattern() {
+    //// get pattern
+    std::string pattern;
+    do {
+        std::cout << "Enter the pattern you want to search for: ";
+        std::getline(std::cin, pattern);
+    } while (pattern.length() < 1);
+
+    std::string input;
+    do {
+        std::cout << "\nYou are searching for: " << pattern << "\n";
+        std::cout << "Is this correct? [y/n]: ";
+        std::getline(std::cin, input);
+        if (input[0] == 'n') return GetPattern();
+    } while (input[0] != 'y');
+
+    return pattern;
+}
+
+void RabinKarp() {
+    Clear(MessageType::RabinKarp);    
+    // text to search
+    std::string text = GetText();
+    // get pattern to search for 
+    std::string pattern = GetPattern();
+    Clear(MessageType::RabinKarp);
+    std::cout << "Searching for: " << pattern;
+}
 
 int main()
 {
+    Clear(MessageType::Menu);
     bool quit = false;
     do {
-
-        std::cout << "Ben Fleuty | CMP 201 Assessment | String search\n";
         std::cout << "Select the algorithm to use:\n";
         std::cout << "1 - Rabin Karp\n";
         std::cout << "2 - Not implemented\n";
@@ -97,14 +140,3 @@ int main()
     } while (!quit);
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
